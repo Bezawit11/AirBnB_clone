4159f9012f123f7fbb173e8c8c01908172a86d5e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-26 12:13:06 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 class BaseModel:
-        """  """
+    """  """
     def __init__(self, *args, **kwargs):
         """  """
         form1 = "%Y-%m-%dT%H:%M:%S.%f"
@@ -13,3 +13,28 @@ class BaseModel:
                 if key == 'id':
                     self.id = value
                 elif key == 'created_at':
+                    self.created_at = datetime.strptime(value, form1)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, form1)
+        else:     
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            
+          
+    def __str__(self):
+        """  """
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+    
+    def save(self):
+        """  """
+        self.updated_at = datetime.now()
+        models.storage.save()
+        
+    def to_dict(self):
+        """ """
+        self.__dict__['__class__'] = __class__.__name__
+        self.__dict__['created_at'] = self.created_at
+        self.__dict__['updated_at'] = self.updated_at
+        return self.__dict__
