fa4dd510075166d1e466146c5fd75ede0e2693eb
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-26 12:34:54 +0000

    Update console.py

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""something"""
+
+import cmd
+class HBNBCommand(cmd.Cmd):
+    """i willll"""
+    def __init__(self):
+       """ """
+        cmd.Cmd.__init__(self)
+        self.prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ """
+        return True
+    
+    def help_quit(self):
+        """  """
+        print("Quit command to exit the program")
+        
+    def do_EOF(self, arg):
+      	"""  """
+        return True
+        
+    def help_EOF(self):
+        """" """
+        print("terminate")
+        
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+        
+    
