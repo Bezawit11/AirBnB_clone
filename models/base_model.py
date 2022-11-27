2b91e6b62f470b75569ff5cc36998719ae6a70fe
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-27 12:21:16 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
-    
+
     def __init__(self, *args, **kwargs):
         """initializes an object
         """
@@ -22,26 +22,25 @@ class BaseModel:
                     self.created_at = datetime.strptime(value, form1)
                 elif key == 'updated_at':
                     self.updated_at = datetime.strptime(value, form1)
-        else:     
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            
-          
+
     def __str__(self):
         """Returns the string representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
-        """updates the public instance attribute 
+        """updates the public instance attribute
            updated_at with the current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
-        
+
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of 
+        """ returns a dictionary containing all keys/values of
             __dict__ of the instance
         """
         d = {**self.__dict__}
